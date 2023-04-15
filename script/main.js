1f8416cb78761a10bc195730dedf06075561342b
doyeon <kiimyeon7@gmail.com>
2023-04-15 14:59:28 +0900

    0415

--- script/main.js
+++ script/main.js
@@ -1,83 +1,67 @@
-// 사진에 mouseover, mouseout 하는 경우
-// 투명한 검은색에 하얀글자가 뜨게 끔  
-// const bImg = document.querySelector('.big_img')
-// const text = document.querySelector('.text')
-// const mImg = document.querySelector('.thum > *')
-// console.log(bImg, mImg, text)
+// 탭 제목 마우스 올리는 이벤트 생성
+// 마우스 올린 대상에 active 클래스 적용
+// 그 외 나머지 초기화 active 클래스 제거 
 
-// bImg.addEventListener('mouseover',()=>{
-//     console.log(bImg)
-    
-// })
-// 탭에 mouseover 했을 때 탭 컨텐츠 나오게 하기 
+const tab_a = document.querySelectorAll('.tab a')
+const active = document.querySelector('.active')
 
+// 내가 작성한 것 //
+/* tab_a.forEach((t,i)=>{
+    t.addEventListener('mouseover',()=>{
+        tab_a[i].classList.add('active') // 클래스 적용
+    })
+    t.addEventListener('mouseout',()=>{
+        tab_a[i].classList.remove('active') // 클래스 제거
+    })
+}) */
 
-// 1. 사진에 mouseover 했을때 검은색 나오게 
-// 2. 사진에 mouseout 했을때 검은색 사라지게 하기 
-
-// 1. 초기값으로 검은색과 글자 숨기기
-const black = document.querySelectorAll('.big_pic span')
-const bImg = document.querySelector ('.big_pic')
-const sImg = document.querySelectorAll('.pic')
-const black2 = document.querySelectorAll('.pic span, .pic .text')
-// const black1 = document.querySelectorAll('.pic1 span, .pic1 .text')
-// const black2 = document.querySelectorAll('.pic2 span, .pic2 .text')
-// const black3 = document.querySelectorAll('.pic3 span, .pic3 .text')
-// const black4 = document.querySelectorAll('.pic4 span, .pic4 .text')
-// const black5 = document.querySelectorAll('.pic5 span, .pic5 .text')
-// const black6 = document.querySelectorAll('.pic6 span, .pic6 .text')
-// const small = document.querySelectorAll('.small')
-console.log(black,bImg,black2,sImg)
-// black.style.opacity = '0';
-
-bImg.addEventListener('mouseover',()=>{
-    console.log(bImg)
-    black[0].style.opacity = '1'
-    black[1].style.opacity = '1'
-})
-
-bImg.addEventListener('mouseout',()=>{
-    console.log(bImg)
-    black[0].style.opacity = '0'
-    black[1].style.opacity = '0'
-})
-
-// 작은 사진 
-// sImg[0].addEventListener('mouseover',()=>{
-//     console.log(sImg[0])
-//     black2[0].style.opacity = '1'
-//     black2[1].style.opacity = '1'
-// })
-
-// sImg[0].addEventListener('mouseout',()=>{
-//     console.log(sImg[0])
-//     black2[0].style.opacity = '0'
-//     black2[1].style.opacity = '0'
-// })
-
-// 작은 사진 반복문 
-// black2[0].style.opacity = '0'
-// black2[1].style.opacity = '0'
-sImg.forEach((i,index)=>{
-    // console.log(i,index)
+for(let i of tab_a){
     i.addEventListener('mouseover',()=>{
-        console.log(i)
-        black2[0].style.opacity = '1'
-        black2[1].style.opacity = '1'
+        for(let j of tab_a){
+            j.classList.remove('active')
+        } // active를 제거하고
+        i.classList.add('active')//마우스 올린 i에 active에 추가하기 
+    })
+}
+
+// 1. txt 모두 숨기기 display=none vs opacity =0
+// 2. .tab a 에 올렸을때 
+// 3. txt 올린대상만 서서히 보이기 opacity = 1
+// 4. 마우스 나가면 사라지기
+
+const tab_c_a = document.querySelectorAll('.tab_c a')
+console.log(tab_c_a)
+console.log(tab_c_a[0].children[0])
+
+// tab_c_a[0].children[0].style.opacity = '0'
+for(let i of tab_c_a){
+    console.log(i, i.children[0])
+    i.children[0].style.opacity = 0
+    i.children[0].style.transition = 'all 0.5s'
+    i.addEventListener('mouseover',()=>{
+        i.children[0].style.opacity = 1
     })
     i.addEventListener('mouseout',()=>{
-        console.log(i)
-        black2[0].style.opacity = '0'
-        black2[1].style.opacity = '0'
+    i.children[0].style.opacity = 0
+    })
+}
+
+// 각 탭 제목에 해당하는 내용만 출력하기 //
+// 초기값 : 모든 내용 숨기기, 1번 내용 보이기
+// 탭 제목에 마우스 올렸을 때 관련 내용 보이기 
+
+const tab_c = document.querySelectorAll('.tab_c')
+// console.log(tab_c)
+// 초기값 
+for(let i of tab_c){i.style.display = 'none'}
+tab_c[0].style.display = 'flex'
+
+
+tab_a.forEach((t,i)=>{
+    t.addEventListener('mouseover',()=>{
+        for(let i of tab_c){i.style.display = 'none'}
+        console.log(t)
+            tab_c [i].style.display = 'flex'
+        // active[i].style.opacity = '1'
     })
 })
-
-// function s_text(){
-//     black2[0].style.opacity = '1'
-//     black2[1].style.opacity = '1'
-// }
-
-
-
-
-// 2. 탭에 mouseover 했을때 tab1, tab2 .. 나오게 하기 
